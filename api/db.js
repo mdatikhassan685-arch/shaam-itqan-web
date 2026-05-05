753da92e3b8f5efc45d0974144835ca010c960bc
mdatikhassan685-arch <mdatikhassan685@gmail.com>
2026-05-05 22:27:03 +0600

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,11 +1,18 @@
+// db.js (Professional Pool Connection)
 import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: 4000,
+    ssl: { rejectUnauthorized: false },
+    waitForConnections: true,
+    connectionLimit: 10, // একসাথে ১০টি কানেকশন হ্যান্ডেল করবে
+    queueLimit: 0
+});
+
 export const getDb = async () => {
-    return await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        port: 4000,
-        ssl: { rejectUnauthorized: false }
-    });
+    return pool; 
 };
